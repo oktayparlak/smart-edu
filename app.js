31e48a5e225776286e3f14e977bfd91e0bceab70
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-10-30 03:59:09 +0300

    project started

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const pageController = require('./controllers/pageController');
 
 const app = express();
 
+/**Template Engine */
+app.set('view engine', 'ejs');
+
+/**Middlewares */
+app.use(express.static('public'));
+
+/**apis */
 app.get('/', pageController.getAllCourses);
+app.get('/about', pageController.getAboutPage);
 
 const port = 4000;
 
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,11 @@
 exports.getAllCourses = (req, res) => {
-    res.send('Index Sayfası')
-}
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+};
+
+exports.getAboutPage = (req, res) => {
+  res.render('about', {
+    page_name: 'about',
+  });
+};
