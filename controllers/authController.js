86a9aa3ae26ddc5285b1c47efbe2647a83464295
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-11-13 16:34:31 +0300

    private middlewares added

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,10 +5,7 @@ exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -26,7 +23,7 @@ exports.loginUser = (req, res) => {
         bcrypt.compare(password, user.password, (err, same) => {
           if (same) {
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('dashboard');
           }
         });
       }
@@ -45,8 +42,10 @@ exports.logoutUser = (req, res) => {
   });
 };
 
-exports.getDashboardPage = (req, res) => {
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
+    user,
   });
 };
